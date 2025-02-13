25a3b8dd3e172d0dae4615b1a7bb300e5bc5e114
concordance <the4pe18@gmail.com>
2025-02-13 14:00:35 +0000

    Include src folder and README.md

--- ops/pipeline/get-image-tag.sh
+++
@@ -1,4 +0,0 @@
-## Update the following line to test changes to CI images
-## See https://xgboost.readthedocs.io/en/latest/contrib/ci.html#making-changes-to-ci-containers
-
-IMAGE_TAG=main
--- tests/cpp/common/test_transform_range.cu
+++
@@ -1,5 +0,0 @@
-/**
- * Copyright 2023 XGBoost contributors
- */
-// Dummy file to keep the CUDA tests.
-#include "test_transform_range.cc"
